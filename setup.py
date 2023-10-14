90e421efc4658a1b31f506d9f006396179276cd3
Kaicheng Yang <yangkc@Kaichengs-Mac-mini.local>
2023-10-13 23:52:01 -0400

    add: long description

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+with open("README.md") as f:
+    long_description = f.read()
+
 setup(
     name="vMFD",
     version="0.1",
     description="Infer moral appeals from text",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url="https://github.com/yangkcatiu/VecOpt",
     author="Kai-Cheng Yang <yang3kc@gmail.com>, Zening Duan <zening.duan@wisc.edu>",
     license="MIT",
