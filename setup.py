331aeb97e317d0edad420b4417368d6b79daa754
Kaicheng Yang <yangkc@Kaichengs-Mac-mini.local>
2023-10-13 23:16:52 -0400

    fix: better doc

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="vMFD",
     version="0.1",
-    description="Infer moral intuitions from text",
+    description="Infer moral appeals from text",
     url="https://github.com/yangkcatiu/VecOpt",
     author="Kai-Cheng Yang <yang3kc@gmail.com>, Zening Duan <zening.duan@wisc.edu>",
     license="MIT",
